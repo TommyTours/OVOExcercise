8687b91eac6e6b5e734d65c7748d36ed97e81816
Tom Clifford <tomclifford@btinternet.com>
2017-11-18 13:30:30 +0000

    WebClient to get JSON implemented.

--- OVOExcercise/Controllers/HomeController.cs
+++ OVOExcercise/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net;
 
 namespace OVOExcercise.Controllers
 {
@@ -11,6 +12,17 @@ namespace OVOExcercise.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult getCustomers()
+        {
+            var url = "https://sheltered-depths-66346.herokuapp.com/customers";
+
+            var syncClient = new WebClient();
+            var content = syncClient.DownloadString(url);
+
             return View();
         }
     }
---
+++ OVOExcercise/Models/CustomerViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OVOExcercise.Models
+{
+    public class CustomerViewModel
+    {
+        private string Id { get; set; }
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        private string Gender { get; set; }
+        private string Title { get; set; }
+    }
+}
