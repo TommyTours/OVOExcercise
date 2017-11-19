ec874604c10f22778593a85016694833bf1b41c2
Tom Clifford <tomclifford@btinternet.com>
2017-11-19 11:06:52 +0000

    Finishing touches added

--- OVOExcercise/Controllers/HomeController.cs
+++ OVOExcercise/Controllers/HomeController.cs
@@ -26,7 +26,9 @@ namespace OVOExcercise.Controllers
             //using the WebClient to get the JSON data
             var content = syncClient.DownloadString(url + "customers");
 
+            //Creating a serializer to convert the data from into a usable format.
             DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(AllCustomers));
+            //Using the serializer to convert the JSON data and create a list of customers that can be used to populate the view.
             using (var ms = new MemoryStream(Encoding.Unicode.GetBytes(content)))
             {
                 var customerData = (AllCustomers)serializer.ReadObject(ms);
@@ -41,16 +43,25 @@ namespace OVOExcercise.Controllers
         [HttpPost]
         public ActionResult Index(HomeViewModel model)
         {
+            //Clearing the model state so that when the view is returned the searchbox will be empty.
+            ModelState.Clear();
+            //using the WebClient to get the JSON data
             var content = syncClient.DownloadString(url + "customer?id=" + model.Id);
 
+            //Creating a serializer to convert the data from into a usable format.
             DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(Customer));
+            //Using the serializer to convert the JSON data and create a list of customers that can be used to populate the view.
             using (var ms = new MemoryStream(Encoding.Unicode.GetBytes(content)))
             {
                 var customerData = (Customer)serializer.ReadObject(ms);
+                //Checking that the submitted value has returned a valid customer, otherwise an error is added to the model state which is the displayed on the view.
+                if (customerData.customer.id == "")
+                {
+                    ModelState.AddModelError("Customer Not Found", "Customer ID: " + model.Id + " not found.");
+                }
                 customerList = new List<CustomerAttributes>();
                 customerList.Add(customerData.customer);
             }
-            
 
             return View(new HomeViewModel() { Customers = customerList });
         }
